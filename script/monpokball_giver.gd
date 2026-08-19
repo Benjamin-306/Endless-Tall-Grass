346ee0a8807c7c2eda370362e5a626243cec11c8
Benjamin-306 <github@benjamin.fudeus.net>
2026-08-19 16:33:40 +0200

    Bugfix time difference Dropper and Anim (#77)
    Fix time difference between Dropper refill and its loading/refill anim

--- script/monpokball_giver.gd
+++ script/monpokball_giver.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
-@onready var animated_sprite = $Sprite2D
+@onready var animated_sprite = $AnimatedSprite2D
 
 var ball = bool(true)
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Timer.start()
+	pass
 	
 
 
@@ -19,6 +19,6 @@ func _on_body_entered(body: Node2D) -> void:
 			body.balls += 1
 
 
-func _on_timer_timeout() -> void:
+func _on_animated_sprite_2d_animation_finished() -> void:
 	ball = true
 	animated_sprite.play("available")
